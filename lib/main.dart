2f85d023fd1ea5013d6718f967a8d3030e92a956
Falghariani <f.alghariani@gmail.com>
2020-01-06 12:06:56 +0200

    Test 3

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         // test application 2
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
