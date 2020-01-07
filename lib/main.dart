9734b298d27059e05eb8c59f30a3d91abdca2d09
Murad Abuseta <muradabuseta@gmail.com>
2020-01-07 09:08:21 +0200

    changed to 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // test application 2
+        // test application 3
 
         // This is the theme of your application.
         //
